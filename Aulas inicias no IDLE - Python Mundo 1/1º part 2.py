ff1681b4f4b615c5a57df9cf1f0e211a7265d4a4
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2020-12-13 14:15:55 -0300

    Add arquivos das Aulas IDLE do Mundo 1 via upload

---
+++ Aulas inicias no IDLE - Python Mundo 1/1º part 2.py
@@ -0,0 +1,4 @@
+nome = input ('Qual é o seu nome ?')
+idade = input ('Quantos anos você tem ?')
+peso = input ('Quanto voce pesa ?')
+print (nome,idade,peso)
---
+++ Aulas inicias no IDLE - Python Mundo 1/1º part 4-(2º Desafio).py
@@ -0,0 +1,7 @@
+print ('Olá, informe de acordo como o solicitado: ')
+dia = input ('Em que dia você nasceu? ')
+mes = input ('Em que mês que nasceu ?')
+ano = input ('Em que ano você nasceu? ')
+print ('Sua data de aniversario é:', dia,'/', mes ,'/', ano ,', Correto?')
+resposta = input ('Insira sua resposta aqui = ')
+print ('Obrigado pelas informações!')
